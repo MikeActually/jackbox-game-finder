8f035098465ebbc60dbeca4f49049332c3c85790
Mike Actually <47235975+MikeActually@users.noreply.github.com>
2020-03-19 19:55:21 -0400

    Updates

---
+++ precache-manifest.97b6393ce903069f855135df7d801668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ac5dc88ffe9e95382f420f7b23f7d8",
+    "url": "/jackbox-game-finder/index.html"
+  },
+  {
+    "revision": "9944570e127c931da7fd",
+    "url": "/jackbox-game-finder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f71449b35b3ec183323",
+    "url": "/jackbox-game-finder/static/js/2.c510f7c3.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/jackbox-game-finder/static/js/2.c510f7c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9944570e127c931da7fd",
+    "url": "/jackbox-game-finder/static/js/main.a8b7f03a.chunk.js"
+  },
+  {
+    "revision": "0a4492b4205a0e34e701",
+    "url": "/jackbox-game-finder/static/js/runtime-main.a8a12906.js"
+  }
+]);
