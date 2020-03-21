b87db075000c383f4d449be64b1f4c5fd7123ffb
Mike Actually <47235975+MikeActually@users.noreply.github.com>
2020-03-20 20:10:50 -0400

    feat(filter): audience filter
    added filtering for audience overflow
    some layout cleanup
    
    Resolves #7

--- src/components/GamesTable/GamesTable.js
+++ src/components/GamesTable/GamesTable.js
@@ -9,6 +9,7 @@ import Paper from '@material-ui/core/Paper';
 import { getGamesList } from '../../data/GamesList/GamesList';
 import GameListHeader from './GamesTableHeader';
 import GameTableFilter from './GameTableFilter';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -35,43 +36,51 @@ export default function GamesTable() {
     const classes = useStyles();
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('pack');
-    const [playerCount, setPlayerCount] = React.useState('playerCount');
+    const [playerCount, setPlayerCount] = React.useState('');
+    const [audience, setAudience] = React.useState(false);
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc';
         setOrder(isAsc ? 'desc' : 'asc');
         setOrderBy(property);
     };
-    const handlePlayerFilter = (event) => {
-        setPlayerCount(event.target.value);
+    const handlePlayerFilter = (value) => {
+        setPlayerCount(value);
+    };
+    const handleAudience = (value) => {
+        setAudience(value);
     };
     return (
-        <div>
-            <GameTableFilter onFilterPlayer={handlePlayerFilter} />
-            <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="simple table">
-                    <GameListHeader
-                        orderBy={orderBy}
-                        order={order}
-                        classes={classes}
-                        onRequestSort={handleRequestSort}
-                    />
-                    <TableBody>
-                        {getGamesList({ order, orderBy, playerCount })
-                            .map((row, index) => {
-                                const labelId = `enhanced-table-checkbox-${index}`;
-                                return (
-                                    <TableRow key={labelId}>
-                                        <TableCell component="th" scope="row">
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell align="right">{row.packName}</TableCell>
-                                        <TableCell align="right">{row.minPlayers}-{row.maxPlayers}</TableCell>
-                                        <TableCell align="right">{row.audience ? 'Yes' : 'No'}</TableCell>
-                                    </TableRow>);
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer >
-        </div>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <GameTableFilter onFilterPlayer={handlePlayerFilter} onAudienceFilter={handleAudience} audience />
+            </Grid>
+            <Grid item xs={12}>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <GameListHeader
+                            orderBy={orderBy}
+                            order={order}
+                            classes={classes}
+                            onRequestSort={handleRequestSort}
+                        />
+                        <TableBody>
+                            {getGamesList({ order, orderBy, playerCount, audience })
+                                .map((row, index) => {
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+                                    return (
+                                        <TableRow key={labelId}>
+                                            <TableCell component="th" scope="row">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="right">{row.packName}</TableCell>
+                                            <TableCell align="right">{row.minPlayers}-{row.maxPlayers}</TableCell>
+                                            <TableCell align="right">{row.audience ? 'Yes' : 'No'}</TableCell>
+                                        </TableRow>);
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer >
+            </Grid>
+        </Grid>
     );
 }
